a73503aaf121c945e4f466ddeb34f3ce827b1436
Olga Meder <aglo6a@gmail.com>
2020-05-17 14:22:35 +0200

    Begin

---
+++ zadanie5_zajecia/cmake-build-debug/CMakeFiles/project_name.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_name.dir/src/cuboid.cpp.o"
+  "CMakeFiles/project_name.dir/src/gnuplot_link.cpp.o"
+  "CMakeFiles/project_name.dir/src/main.cpp.o"
+  "project_name"
+  "project_name.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zadanie5_zajecia/src/cuboid.cpp
@@ -0,0 +1,45 @@
+#include "cuboid.hh"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int Cuboid::counter =5;
+
+Cuboid::Cuboid(): angle{0}
+{
+    ifstream inputFile;
+    inputFile.open(kModelCuboid);
+    if(!inputFile.is_open())
+    {
+        cerr << "Unable to load model Cuboid file!" 
+             << endl;
+        return;
+    }
+
+    Vector3D point;
+    while(inputFile >> point)
+    {
+        points.push_back(point);
+    }
+    inputFile.close();
+}
+
+void Cuboid::draw(std::string filename) const
+{
+    ofstream outputFile;
+    outputFile.open(filename);
+    if(!outputFile.is_open())
+    {
+        cerr << "Unable to open drone file!" << endl;
+        return;
+    }
+    for(int i = 0; i < points.size(); ++i)
+    {
+        outputFile << points[i]+ translation;
+        if(i % 4 == 3) // triggers after every 4 points
+        {
+            outputFile << "#\n\n";
+        }
+    }
+}
